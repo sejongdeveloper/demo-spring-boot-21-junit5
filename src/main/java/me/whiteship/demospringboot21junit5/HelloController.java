09db4cc7cf1790219ceddc9779b3a0b69f39d9d4
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-09 22:10:54 +0900

    JUnit 5

---
+++ src/main/java/me/whiteship/demospringboot21junit5/HelloController.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospringboot21junit5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
